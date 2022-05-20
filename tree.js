96d618c0f582afa388235f47b993369fd15949fd
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-19 22:00:22 -0400

    write sumValues

--- tree.js
+++ tree.js
@@ -1,35 +1,42 @@
 /** TreeNode: node for a general tree. */
 
 class TreeNode {
-  constructor(val, children = []) {
-    this.val = val;
-    this.children = children;
-  }
+	constructor(val, children = []) {
+		this.val = val;
+		this.children = children;
+	}
 }
 
 class Tree {
-  constructor(root = null) {
-    this.root = root;
-  }
+	constructor(root = null) {
+		this.root = root;
+	}
 
-  /** sumValues(): add up all of the values in the tree. */
+	/** sumValues(): add up all of the values in the tree. */
 
-  sumValues() {
-    
-  }
+	sumValues(total = 0) {
+		let queue = [ this.root ];
+		console.log(queue, queue.length);
+		while (queue.length) {
+			const current = queue.shift();
+			if (current !== null) {
+				total += current.val;
+				for (let child of current.children) {
+					queue.push(child);
+				}
+			}
+		}
+		return total;
+	}
 
-  /** countEvens(): count all of the nodes in the tree with even values. */
+	/** countEvens(): count all of the nodes in the tree with even values. */
 
-  countEvens() {
+	countEvens() {}
 
-  }
-
-  /** numGreater(lowerBound): return a count of the number of nodes
+	/** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
-  numGreater(lowerBound) {
-
-  }
+	numGreater(lowerBound) {}
 }
 
 module.exports = { Tree, TreeNode };
